ce66faaba862af3c9d70bc79a3e956c2764f2e34
Yash Jain <yash.199301055@muj.manipal.edu>
2021-06-14 14:14:58 +0000

    Completed the mini project according to the instructions

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if(principal<=0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit "+principal+", \<br\>at an amount rate of "+rate+"%\<br\>You will receive an amount of "+amount+", \<br\>in the year "+year+"\<br\>";
+}
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
